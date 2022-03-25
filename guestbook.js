ad948c901de5bbad3febf915b72baa02fd625521
mohamedalidridii <96180127+mohamedalidridii@users.noreply.github.com>
2022-03-25 14:44:01 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://c79af4e0.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
